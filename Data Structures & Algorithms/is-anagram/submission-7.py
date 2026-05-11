d544a84201e51e54643f585ee510cffcd990aa23
Hashim Khan <khan521@purdue.edu>
2026-05-11 12:26:19 -0400

    Add: is-anagram - submission-7

---
+++ Data Structures & Algorithms/is-anagram/submission-7.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        s1 = [0] * 26
+        t1 = [0] * 26
+
+        for i in range(len(s)):
+            s1[ord(s[i]) - ord('a')]+=1
+            t1[ord(t[i]) - ord('a')]+=1
+
+        return s1 == t1
