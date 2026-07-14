e2c4d756b64f4b1602f58193319d56c31146a189
Hashim Khan <khan521@purdue.edu>
2026-07-14 15:00:33 -0400

    Add: extra-characters-in-a-string - submission-6

---
+++ Data Structures & Algorithms/extra-characters-in-a-string/submission-6.py
@@ -0,0 +1,17 @@
+class Solution:
+    def minExtraChar(self, s: str, dictionary: List[str]) -> int:
+        words = set(dictionary)
+        memo = {}
+        def dfs(i):
+            if i == len(s):
+                return 0
+            if i in memo:
+                return memo[i]
+            res = 1 + dfs(i+1)
+
+            for j in range(i, len(s)):
+                if s[i: j+1] in words:
+                    res = min(res, dfs(j+1))
+            memo[i] = res
+            return res
+        return dfs(0)
