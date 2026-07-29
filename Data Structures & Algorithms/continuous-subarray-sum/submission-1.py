ec74237e14b26f430e588de06aa578adb58a01de
Hashim Khan <khan521@purdue.edu>
2026-07-29 10:21:34 -0400

    Add: continuous-subarray-sum - submission-1

---
+++ Data Structures & Algorithms/continuous-subarray-sum/submission-1.py
@@ -0,0 +1,10 @@
+class Solution:
+    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        for i in range(len(nums)):
+            cur = nums[i]
+            for j in range(i+1, len(nums)):
+                cur = cur + nums[j]
+                if cur % k == 0:
+                    return True
+            cur = nums[i]
+        return False
