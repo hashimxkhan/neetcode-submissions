c956189d04a0c5bbe1664af8c5ef060901815fea
Hashim Khan <khan521@purdue.edu>
2026-08-09 19:41:01 -0400

    Add: maximum-twin-sum-of-a-linked-list - submission-2

---
+++ Data Structures & Algorithms/maximum-twin-sum-of-a-linked-list/submission-2.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def pairSum(self, head: Optional[ListNode]) -> int:
+        arr = []
+        cur = head
+        while cur:
+            arr.append(cur.val)
+            cur = cur.next
+        
+        l = 0
+        r = len(arr) - 1
+        best = 0
+        while l < r:
+            best = max(best, arr[l] + arr[r])
+            l+=1
+            r-=1
+        return best
