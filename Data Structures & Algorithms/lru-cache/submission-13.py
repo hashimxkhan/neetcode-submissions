6c4cfa68a6c4bbc49be0a0f05f2184c51bf1a21d
Hashim Khan <khan521@purdue.edu>
2026-09-01 15:42:45 -0400

    Add: lru-cache - submission-13

---
+++ Data Structures & Algorithms/lru-cache/submission-13.py
@@ -0,0 +1,26 @@
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = OrderedDict()
+        self.size = 0
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        self.cache.move_to_end(key)
+        return self.cache[key]
+        
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.cache[key] = value
+            self.cache.move_to_end(key)
+            return
+        
+        self.cache[key] = value
+        self.size+=1
+
+        if self.size > self.capacity:
+            self.cache.popitem(last=False)
+
+
