c96777cd2cb91fe2f85dc62d8da0a2cda91670de
Hashim Khan <khan521@purdue.edu>
2026-09-22 12:38:33 -0400

    Add: valid-palindrome-ii - submission-0

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-0.py
@@ -0,0 +1,21 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        
+
+        def isPalindrome(s):
+            l = 0
+            r = len(s) - 1
+            while l < r:
+                if s[l] != s[r]:
+                    return False
+                l+=1
+                r-=1
+            return True
+        
+        if isPalindrome(s):
+            return True
+        
+        for i in range(len(s)):
+            if isPalindrome(s[:i] + s[i+1:]):
+                return True
+        return False
